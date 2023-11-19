f9b0f0fa034e01e550d5e80e8f6e54ff055283d0
LAPTOP-8VRBF9B1\kleur <karinjoyce91@gmail.com>
2023-11-19 17:14:56 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,27 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-        <form name="test" method="POST" data-netlify="true">
-          <p><label>HEJHEJ: <input type="text" name="name"></input></label></p>
-        </form>
+        <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
       </main>
 
       <Footer />
