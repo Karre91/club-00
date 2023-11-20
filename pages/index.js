9ffc74e9e8aa03c24269fb474ae242a62cfa41a8
LAPTOP-8VRBF9B1\kleur <karinjoyce91@gmail.com>
2023-11-20 19:43:25 +0100

    Add email.

--- pages/index.js
+++ pages/index.js
@@ -6,26 +6,24 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        {/* <title>CLUBCLUB</title>
+        <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       <form name="contact v1" method='POST' data-netlify='true' onSubmit='submit'>
-<input type='hidden' name='form-name' value='contact v1'/>
+      <input type='hidden' name='form-name' value='contact v1'/>
 
-<label>NAME
-  <input type='text' name='name'/>
-</label>
-<button type='submit'>SUBBE</button>
+      <label>Name: 
+        <input type='text' name='name'/>
+      </label>
+      <label>Email: 
+        <input type='text' name='name'/>
+      </label>
+      <button type='submit'>SUBBE</button> 
 
       </form>
       </main>
-
       <Footer />
     </div>
   )
